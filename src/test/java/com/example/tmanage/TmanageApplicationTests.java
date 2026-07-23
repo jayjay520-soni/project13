7cb1f7783c241b4857d590cdb526ac8324e16a94
chenyihan <chenyihan@chenyihan.com>
2026-07-23 17:13:53 +0800

    feat: 初始化tmanage项目

---
+++ src/test/java/com/example/tmanage/TmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
